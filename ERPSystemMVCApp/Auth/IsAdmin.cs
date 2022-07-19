fab44fc4c4f9dffdc549230967b9e0ec05ce8ff1
mohibor <mohibor.rahat@outlook.com>
2022-07-20 02:30:09 +0600

    Add project files.

---
+++ ERPSystemMVCApp/Auth/IsAdmin.cs
@@ -0,0 +1,34 @@
+﻿using ERPSystemMVCApp.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ERPSystemMVCApp.Auth
+{
+    public class IsAdmin : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            User sessionUser = null;
+
+            if(filterContext.HttpContext.Session["user"] != null)
+            {
+                sessionUser = (User)filterContext.HttpContext.Session["user"];
+            }
+
+            if (!(sessionUser != null && sessionUser.Type <= 1))
+            {
+                filterContext.Controller.TempData["error_message"] = "Your not Admin or System User";
+
+                filterContext.Result = new RedirectToRouteResult(
+                    new RouteValueDictionary(new { controller = "Dashboard", action = "Index" })
+                );
+
+                filterContext.Result.ExecuteResult(filterContext.Controller.ControllerContext);
+            }
+        }
+    }
+}
---
+++ ERPSystemMVCApp/Controllers/HomeController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ERPSystemMVCApp.Auth;
+using ERPSystemMVCApp.EF;
+using ERPSystemMVCApp.Models;
+
+namespace ERPSystemMVCApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        [HttpGet, NotLoggedIn]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(UserLoginModel user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(user);
+            }
+
+            var db = new ERPSystemDBEntities1();
+            var db_user = db.Users.Where(u => u.Email == user.Email).SingleOrDefault();
+
+            if (db_user == null)
+            {
+                TempData["error_message"] = "Invalid email or password";
+                return RedirectToAction("Login", "Home");
+            }
+
+            bool verified_check = db_user.Verified == 1;
+
+            if (!verified_check)
+            {
+                TempData["error_message"] = "Your account is not verified yet";
+                return RedirectToAction("Login", "Home");
+            }
+
+            bool password_check = db_user.Password == user.Password;
+
+            if (!password_check)
+            {
+                TempData["error_message"] = "Invalid email or password";
+                return RedirectToAction("Login", "Home");
+            }
+
+            db_user.Status = 1;
+
+            db.WorkingHours.Add(new WorkingHour
+            {
+                UserId = db_user.Id,
+                Date = DateTime.Now.Date,
+                EntryTime = DateTime.Now,
+            }
+            );
+
+            Session["user"] = db_user;
+            Session["loggedin"] = true;
+
+            db.SaveChanges();
+
+            return RedirectToAction("Index", "Dashboard");
+        }
+
+        public ActionResult Logout()
+        {
+            var sessionUser = (User)Session["user"];
+            if (sessionUser != null)
+            {
+                var db = new ERPSystemDBEntities1();
+                var sessionUserId = sessionUser.Id;
+
+                var db_working_hour = db.WorkingHours.Where(w => w.UserId == sessionUserId && w.ExitTime == null).SingleOrDefault();
+
+                if (db_working_hour != null)
+                {
+                    db_working_hour.ExitTime = DateTime.Now;
+                    db.SaveChanges();
+                }
+
+                var user = db.Users.Where(t => t.Id == sessionUserId).SingleOrDefault();
+
+                user.Status = 0;
+
+                db.SaveChanges();
+            }
+
+            Session.RemoveAll();
+
+            return RedirectToAction("Login", "Home");
+        }
+    }
+}
